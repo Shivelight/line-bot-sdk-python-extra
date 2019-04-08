5a5d6d7e10893026e744ef1bce1d56123ee64aa8
Arkie <cwkfr@protonmail.com>
2019-04-08 20:20:46 +0700

    :heavy_plus_sign: Add aiohttp as dependency.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'line-bot-sdk',
+    'line-bot-sdk', 'aiohttp'
 ]
 
 # What packages are optional?
